8dbaffe704bf5867a6b9ccb62852e960598bca13
ruzny <ruzny@ruzny.com>
2020-06-12 17:21:33 +0530

    changed genericsAPIViews to viewsets for language app

--- src/language/api/api.py
+++ src/language/api/api.py
@@ -1,39 +1,12 @@
-from rest_framework.views import APIView
-from rest_framework.generics import (
-    ListAPIView,
-    RetrieveAPIView,
-    CreateAPIView,
-    UpdateAPIView,
-    DestroyAPIView
-)
-from rest_framework import permissions
+from rest_framework import viewsets, permissions
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 
 from language.serializer import LanguageSerializer
 from language.models import Language
 
-class LanguageListAPI(ListAPIView):
-    queryset            = Language.objects.all()
-    serializer_class    = LanguageSerializer
 
-class LanguageCreateAPI(CreateAPIView):
-    queryset            = Language.objects.all()
-    serializer_class    = LanguageSerializer
-    permission_classes  = [permissions.IsAuthenticated]
-
-class LanguageRetrieveAPI(RetrieveAPIView):
-    queryset            = Language.objects.all()
-    serializer_class    = LanguageSerializer
-    permission_classes  = [permissions.IsAuthenticated]
-    # lookup_field = 'id'
-
-class LanguageUpdateAPIView(UpdateAPIView):
-    queryset            = Language.objects.all()
-    serializer_class    = LanguageSerializer
-    permission_classes  = [permissions.IsAuthenticated]
-
-class LanguageDestroyAPIView(DestroyAPIView):
-    queryset            = Language.objects.all()
-    serializer_class    = LanguageSerializer
-    permission_classes  = [permissions.IsAuthenticated]
+class LanguageAPIView(viewsets.ModelViewSet):
+    queryset = Language.objects.all()
+    serializer_class = LanguageSerializer
+    permission_classes = [permissions.AllowAny]
--- src/language/urls.py
+++ src/language/urls.py
@@ -1,18 +1,9 @@
 from django.urls import path
-from .api.api import (
-    LanguageListAPI,
-    LanguageCreateAPI,
-    LanguageRetrieveAPI,
-    LanguageUpdateAPIView,
-    LanguageDestroyAPIView
-)
+from rest_framework import routers
+from .api.api import LanguageAPIView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register('language', LanguageAPIView, 'language')
 
 app_name    = 'language'
-urlpatterns = [
-    path('', LanguageListAPI.as_view(), name='language-list'),
-    path('create', LanguageCreateAPI.as_view(), name='language-create'),
-    path('<int:pk>', LanguageRetrieveAPI.as_view(), name='language-retrieve'),
-    path('<int:pk>/update', LanguageUpdateAPIView.as_view(), name='language-update'),
-    path('<int:pk>/destroy', LanguageDestroyAPIView.as_view(), name='language-destroy'),
-
-]
+urlpatterns = router.urls
