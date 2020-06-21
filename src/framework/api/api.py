cfdcdcbc2f185fe8f4d6dd47312d015a95393c28
ruzny <ruzny@ruzny.com>
2020-06-21 20:29:29 +0530

    refactored some code

--- src/framework/api/api.py
+++ src/framework/api/api.py
@@ -1,5 +1,8 @@
 from rest_framework.views import APIView
 from django.shortcuts import get_object_or_404
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 from rest_framework.generics import (
     ListAPIView,
     CreateAPIView,
@@ -7,9 +10,6 @@ from rest_framework.generics import (
     UpdateAPIView,
     DestroyAPIView
 )
-from rest_framework import permissions
-from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 
 from framework.serializer import FrameworkSerializer
 from framework.models import Framework
