08b07443c1090c9736d94d5248003365ac3d7f6c
ruzny <ruzny@ruzny.com>
2020-06-23 21:41:40 +0530

    setup user authentication

--- src/account/api/api.py
+++ src/account/api/api.py
@@ -20,3 +20,20 @@ class RegisterAPIView(generics.GenericAPIView):
             ).data,
             "token": AuthToken.objects.create(user)[1],
         })
+
+
+# AUTHENTICATE API VIEW
+class AuthenticateAPIView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        return Response({
+            "user": UserSerializer(
+                user,
+                context=self.get_serializer_context()
+            ).data,
+            "token": AuthToken.objects.create(user)[1],
+        })
--- src/account/urls.py
+++ src/account/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .api.api import RegisterAPIView
+from .api.api import RegisterAPIView, AuthenticateAPIView
 
 app_name = 'account'
 urlpatterns = [
     path('register', RegisterAPIView.as_view(), name='register'),
+    path('login', AuthenticateAPIView.as_view(), name='authenticate'),
 ]
