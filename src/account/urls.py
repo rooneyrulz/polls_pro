7783cd5b6b24f501493075986221299ce11062d2
ruzny <ruzny@ruzny.com>
2020-06-23 21:53:14 +0530

    setup logout

--- src/account/urls.py
+++ src/account/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from knox import views as knox_view
 from .api.api import RegisterAPIView, AuthenticateAPIView, UserAPIView
 
 app_name = 'account'
@@ -6,4 +7,5 @@ urlpatterns = [
     path('register', RegisterAPIView.as_view(), name='register'),
     path('login', AuthenticateAPIView.as_view(), name='authenticate'),
     path('user', UserAPIView.as_view(), name='auth_user'),
+    path('logout', knox_view.LogoutView.as_view(), name='knox_logout'),
 ]
