bac9bff307c72da2d74ba9865ea311f1522193aa
ruzny <ruzny@ruzny.com>
2020-06-23 21:35:42 +0530

    working on user authentication

--- src/account/serializer.py
+++ src/account/serializer.py
@@ -25,4 +25,10 @@ class RegisterSerializer(serializers.ModelSerializer):
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError('Invalid Credentials')
         
