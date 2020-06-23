63481464521930287e37a636063d2db9863c0cce
ruzny <ruzny@ruzny.com>
2020-06-23 20:22:40 +0530

    working on user registration

--- src/account/serializer.py
+++ src/account/serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 # USER SERIALIZER
 class UserSerializer(serializers.ModelSerializer):
@@ -12,6 +13,10 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        print(validated_data)
         
 
 # LOGIN SERIALIZER
