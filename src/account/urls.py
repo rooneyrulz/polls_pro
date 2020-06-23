247547434029bc749105bcee0ebad0841692d01d
ruzny <ruzny@ruzny.com>
2020-06-23 21:47:58 +0530

    setup auth user api

--- src/account/urls.py
+++ src/account/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .api.api import RegisterAPIView, AuthenticateAPIView
+from .api.api import RegisterAPIView, AuthenticateAPIView, UserAPIView
 
 app_name = 'account'
 urlpatterns = [
     path('register', RegisterAPIView.as_view(), name='register'),
     path('login', AuthenticateAPIView.as_view(), name='authenticate'),
+    path('user', UserAPIView.as_view(), name='auth_user'),
 ]
