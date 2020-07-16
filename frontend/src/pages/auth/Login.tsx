b1e4963436fd8344cb7598e00b15924a124d2b0b
ruzny <ruzny@ruzny.com>
2020-07-16 21:48:10 +0530

    working on login comp

--- frontend/src/pages/auth/Login.tsx
+++ frontend/src/pages/auth/Login.tsx
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // BOOTSTRAP
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 const Login: React.FC = () => {
+  const [formData, setFormData] = useState({ email: '', password: '' });
+
+  const onChange = (e: any) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e: any) => console.log(formData);
+
+  const { email, password } = formData;
+
   return (
     <div className='Login'>
       <div className='login-header'>
         <h2>Let's Login</h2>
       </div>
       <hr />
-      <Form>
+      <Form onSubmit={(e) => onSubmit(e)}>
         <Form.Group controlId='formBasicEmail'>
           <Form.Label>Email</Form.Label>
-          <Form.Control type='email' placeholder='Enter email' />
+          <Form.Control
+            type='email'
+            name='email'
+            value={email}
+            placeholder='Enter email'
+            onChange={(e) => onChange(e)}
+          />
           <Form.Text className='text-muted'>
             We'll never share your email with anyone else.
           </Form.Text>
@@ -22,7 +37,13 @@ const Login: React.FC = () => {
 
         <Form.Group controlId='formBasicPassword'>
           <Form.Label>Password</Form.Label>
-          <Form.Control type='password' placeholder='Password' />
+          <Form.Control
+            type='password'
+            name='password'
+            value={password}
+            placeholder='Password'
+            onChange={(e) => onChange(e)}
+          />
         </Form.Group>
         <Form.Group controlId='formBasicCheckbox'>
           <Form.Check type='checkbox' label='Check me out' defaultChecked />
